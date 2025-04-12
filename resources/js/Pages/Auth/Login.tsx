d3ff349ae4e103183622dd017dad70d0884053cc
Wandile-cyber <ralphdlamini3@gmail.com>
2025-04-12 23:12:51 +0200

    initialize nyx

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -48,7 +48,7 @@ export default function Login({
     return (
         <>
             <GuestLayout>
-                <Card>
+                <Card className="bg-white">
                     <CardContent>
                         <Head title="Log in" />
 
